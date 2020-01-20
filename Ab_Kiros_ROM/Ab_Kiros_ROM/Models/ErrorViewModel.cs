11d98bf4129054b7db9f05fe54cefb870920b5ec
AbRetroDev <akiros@retrorabbit.co.za>
2020-01-20 13:35:34 +0200

    Adding Vs Project

---
+++ Ab_Kiros_ROM/Ab_Kiros_ROM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ab_Kiros_ROM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Ab_Kiros_ROM/Ab_Kiros_ROM/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ab_Kiros_ROM.Models
+{
+    public class Person
+    {
+      
+            public Guid person_ID { get; set; }
+            public string first_name { get; set; }
+            public string surname { get; set; }
+            public string gender { get; set; }
+            public string Id_Number { get; set; }
+            public string email { get; set; }
+            public string contact_No { get; set; }
+            public char active { get; set; }
+
+        
+    }
+}
